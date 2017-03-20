b32d5c7027c6cf784fd4963fa576bbe619664a32
Oliver Kett <oliver.kett@fau.de>
2017-03-20 08:36:53 +0100

    Initial Commit

---
+++ networkShareMounter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  networkShareMounter
+//
+//  Created by Kett, Oliver on 20.03.17.
+//  Copyright © 2017 Kett, Oliver. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
